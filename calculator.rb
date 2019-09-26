16a96de7f8becc61f8ccef84597fe4d25671c4c1
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-26 19:18:55 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,22 @@
-# Add your variables here
+first_number = 9
+
+second_number = 3
+
+
+def sum  
+  puts first_number + second_number
+end
+
+def difference
+  puts first_number - second_number
+end
+
+def product  
+  puts first_number * second_number
+end
+
+def quotient 
+  puts first_number / second_number
+end
+
+
