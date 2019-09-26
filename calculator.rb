9993cb3759005834e07e957416f1c9933ccfacc0
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-26 19:20:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,15 +4,11 @@ second_number = 3
 
 
 sum = first_number + second_number
-end
 
 difference = first_number - second_number
-end
 
 product = first_number * second_number
-end
 
 quotient = first_number / second_number
-end
 
 
