70d768374515ba65689e713910bf8c423346070c
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-26 19:19:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,20 +3,16 @@ first_number = 9
 second_number = 3
 
 
-def sum  
-  puts first_number + second_number
+sum = first_number + second_number
 end
 
-def difference
-  puts first_number - second_number
+difference = first_number - second_number
 end
 
-def product  
-  puts first_number * second_number
+product = first_number * second_number
 end
 
-def quotient 
-  puts first_number / second_number
+quotient = first_number / second_number
 end
 
 
